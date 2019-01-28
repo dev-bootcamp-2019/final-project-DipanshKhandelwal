bcb7040ae4486d5a877a438d0bbe4328ebc039e0
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-01-28 15:20:28 +0530

    Add test to check if new user is created or not

---
+++ test/coffeehouse.test.js
@@ -0,0 +1,29 @@
+const CoffeeHouse = artifacts.require("./CoffeeHouse.sol")
+
+contract('CoffeeHouse', accounts => {
+  const owner = accounts[0]
+  const user1 = accounts[1]
+  const user2 = accounts[2]
+  const testUser = accounts[3]
+
+  // A test to create a user, this contract would test if user is able to register successfully
+  it('Should create a new user', async () => {
+    // Instance of the deployed contract
+    const coffeeHouse = await CoffeeHouse.deployed()
+
+    // Call the createUser function to save the user info in the blockchain
+    await coffeeHouse.createUser('user1', { from: user1 });
+    await coffeeHouse.createUser('user2', { from: user2 });
+
+    // Get the created user
+    const userData = await coffeeHouse.users.call(user1)
+
+    // Destructure the details of the created user
+    const { name, userAddress } = userData
+
+    // Run assertions for all the user fields and verify correctness
+    assert.equal(user1, userAddress, 'Address of the created user is incorrect')
+    assert.equal('user1', name, 'Username of the created user is incorrect')
+  });
+
+})
