52fb6d93c9cc6dd933664e3092573e7ce35a3c02
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-01-28 13:18:48 +0530

    Deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var CoffeeHouse = artifacts.require("./CoffeeHouse.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(CoffeeHouse);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,11 @@
 const path = require("path");
+const fs = require('fs');
+const fs2 = require('fs');
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const infuraKey = fs.readFileSync(".secret").toString().trim();
+const mnemonic = fs2.readFileSync(".mnemonic").toString().trim();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -11,5 +18,10 @@ module.exports = {
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
      },
+     rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,       // Ropsten's id
+    },
   }
+
 };
